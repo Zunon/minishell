f79fee50dcbc9431db98f4efe738dad2c2ec3c13
Radi <radiriyas@gmail.com>
2023-01-02 23:24:46 +0400

    Revised code with command structs

--- lib/libds/hashtable.h
+++ lib/libds/hashtable.h
@@ -6,7 +6,7 @@
 /*   By: rriyas <rriyas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 11:58:02 by rriyas            #+#    #+#             */
-/*   Updated: 2023/01/02 10:35:29 by rriyas           ###   ########.fr       */
+/*   Updated: 2023/01/02 11:42:16 by rriyas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,15 +41,18 @@ typedef struct s_hash_table{
     t_pair **table;
 } t_hash_table;
 
+// hash_table.c
 t_hash_table *create_ht(int s);
-void destroy_ht(t_hash_table *t);
-void destroy_pair(t_pair *p);
-t_hash_table *resize_ht(t_hash_table *t, int);
 t_pair *retrieve_from_ht(t_hash_table *t, char *key);
-void insert_into_ht(t_hash_table **t, char* key, char *value);
+void insert_into_ht(t_hash_table **t, char *key, char *value);
 void remove_from_ht(t_hash_table *t, char *key);
+
+//hash_table_utils.c
+void destroy_pair(t_pair *p);
+void destroy_ht(t_hash_table *t);
+void print_pair(t_pair *p);
 void print_ht(t_hash_table *t);
-int hash(char *);
+t_hash_table *resize_ht(t_hash_table *t, int newCapacity);
 
 
 #endif
