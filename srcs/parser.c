eab89aedd2aadd0f62b8f441998e2789a32e635e
kalmheir <zunon@zunon.io>
2023-01-14 15:53:34 +0400

    Recursive Descent Parser

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,11 +1,69 @@
 #include "../inc/minishell.h"
 
-t_command   *parse_input(const char *input)
-{
-    t_token *list;
-
-    list = tokenize((char *)input);
-    // @TODO: Recursive Descent Parsing Using our Grammar rules
-    return (0);
+t_bool parse_redirection(t_token **tracer) {
+	if (!(*tracer)->next || (*tracer)->next->type != WORD)
+		return FALSE;
+	*tracer = (*tracer)->next;
+	return TRUE;
 }
 
+t_bool parse_redirection_list(t_token **tracer) {
+	if (!parse_redirection(tracer))
+		return FALSE;
+	if ((*tracer)->next && (*tracer)->next->type == REDIRECTION) {
+		*tracer = (*tracer)->next;
+		return parse_redirection_list(tracer);
+	}
+	return TRUE;
+}
+
+t_bool parse_simple_command_element(t_token **tracer) {
+	if ((*tracer)->type == REDIRECTION)
+		return parse_redirection_list(tracer);
+	if ((*tracer)->type == WORD)
+		return TRUE;
+	return FALSE;
+}
+
+t_bool parse_simple_command(t_token **tracer) {
+	if (!parse_simple_command_element(tracer)) {
+		return FALSE;
+	}
+	if ((*tracer)->next) {
+		*tracer = (*tracer)->next;
+		if (parse_simple_command(tracer))
+			return TRUE;
+		*tracer = (*tracer)->prev;
+	}
+	return TRUE;
+}
+
+t_bool parse_pipeline(t_token *list) {
+	t_token *tracer;
+
+	tracer = list;
+	if (!parse_simple_command(&tracer))
+		return FALSE;
+	if (tracer->next && tracer->next->type == PIPE)
+	{
+		if (!(tracer->next->next))
+			return FALSE;
+		tracer = tracer->next->next;
+		return parse_pipeline(tracer);
+	}
+	return TRUE;
+}
+
+t_command	*parse_input(const char *input)
+{
+	t_token *list;
+
+	list = tokenize((char *)input);
+	print_tokens(list);
+	if (!parse_pipeline(list)) {
+		ft_printf("Parse Error!\n");
+		return (NULL);
+	}
+	ft_printf("Success!\n");
+	return (0);
+}
